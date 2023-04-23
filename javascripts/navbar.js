b8a13e4c69677c4ac0de56688c1bd87aa4bc192f
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-04-22 19:38:14 -0500

    add navbar responsiveness
    - added navbar responsiveness
    - slight styling changes

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -6,21 +6,22 @@ class NavBar extends HTMLElement
         this.innerHTML = 
             `<nav>
                 <div class="logo-container">
-                <a href="/todd-spainhour-individual-assignment-3-swe-5063/welcome.html" class="clickable-logo">
-                    <img id="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRslKnNvO72QK-3TrnGRVeyHTaEJHHSxsoFCkHw3Yv5rQ&usqp=CAU&ec=48665699"></img>
-                    <h1>Victory Board Games</h1>
-                </a>
+                    <a href="/todd-spainhour-individual-assignment-3-swe-5063/welcome.html" class="clickable-logo">
+                        <img id="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRslKnNvO72QK-3TrnGRVeyHTaEJHHSxsoFCkHw3Yv5rQ&usqp=CAU&ec=48665699"></img>
+                        <h1>Victory Board Games</h1>
+                    </a>
                 </div>
-            
-                <div class="nav-element">
-                    <a href="welcome.html">Welcome</a>
+                <div class="nav-element-container">
+                    <div class="nav-element">
+                        <a href="welcome.html">Welcome</a>
+                    </div>
+                    <div class="nav-element">
+                        <a href="products.html" class="nav-element">Products</a>
+                    </div>
+                    <div class="nav-element">
+                        <a href="about.html" class="nav-element">About Us</a>
+                    </div>  
                 </div>
-                <div class="nav-element">
-                    <a href="products.html" class="nav-element">Products</a>
-                </div>
-                <div class="nav-element">
-                    <a href="about.html" class="nav-element">About Us</a>
-                </div>       
             </nav>`;
     }
 }
